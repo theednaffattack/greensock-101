28c141de3719a07c73da025af6c6872adeb174e6
Eddie Naff <eddienaff@gmail.com>
2017-08-17 20:53:22 -0700

    Finish simple callback functions

--- 04-SimpleCallbackFunctions/start/src/js/main.js
+++ 04-SimpleCallbackFunctions/start/src/js/main.js
@@ -1,9 +1,27 @@
 (function($) {
     
-	var img = $('img'),
-		h2 = $('h2');
+	const img = $('img');
+	const	h2 = $('h2');
+  let i = 0;
 
 	// Simple Callback Functions
-	TweenLite.from(img, 1, {x: -200, ease:Power1.easeInOut});
+	TweenLite.from(img, 1, {
+    x: -200,
+    ease:Power1.easeInOut,
+    onStart: onStart,
+    onUpdate: onUpdate,
+    onComplete: onComplete,
+  });
 
+  function onStart() {
+    console.log('animation is starting');
+  }
+
+  function onUpdate() {
+    h2.text(i++);
+  }
+  
+  function onComplete() {
+    console.log('animation is completed');
+  }
 })(jQuery);
