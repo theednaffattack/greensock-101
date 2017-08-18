5f646ff7f7c7836abaceaf2560d5084337e9655d
Eddie Naff <eddienaff@gmail.com>
2017-08-17 21:33:28 -0700

    Finish timeline / play control &  button examples

--- 07-Control-Timeline-Playback/start/src/js/main.js
+++ 07-Control-Timeline-Playback/start/src/js/main.js
@@ -9,12 +9,51 @@
 
 	// Controlling Timeline Playback
 	tl
-		.from(h1, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut})
+		.from(h1, 1, {y: -15, autoAlpha: 0, ease:Power1.easeOut})
 		.add('intro')
-		.from(intro, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut})
-		.from(img, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut}, 'intro')
-		.from(h2, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut}, 'intro+=3')
-		.from(listItem, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut}, 'intro');
+		.from(intro, 1, {y: -15, autoAlpha: 0, ease:Power1.easeOut})
+		.from(img, 1, {y: -15, autoAlpha: 0, ease:Power1.easeOut})
+		.from(h2, 1, {y: -15, autoAlpha: 0, ease:Power1.easeOut})
+		.from(listItem, 1, {y: -15, autoAlpha: 0, ease:Power1.easeOut});
+
+	tl.pause();
+
+	$('#btnPlay').on('click', function() {
+		tl.play();
+	});
+
+	$('#btnPause').on('click', function() {
+		tl.pause();
+	});
+
+	$('#btnResume').on('click', function() {
+		tl.resume();
+	});
+
+	$('#btnReverse').on('click', function() {
+		tl.reverse();
+	});
+
+	$('#btnSpeedUp').on('click', function() {
+		tl.timeScale(8); // 8 times the original speed
+	});
+
+	$('#btnSlowDown').on('click', function() {
+		tl.timescale(0.5); // 50% of the original speed
+	});
+
+	$('#btnSeek').on('click', function() {
+		tl.seek(1); // this jumps to an absolute position in the timeline. takes seconds or labels
+	});
+
+	// go to 50% button on the screen
+	$('#btnProgress').on('click', function() {
+		tl.progress(0.5); // it leaps to this point on the timeline 1 = 100%
+	});
+
+	$('#btnRestart').on('click', function() {
+		tl.restart();
+	});
 
 })(jQuery);
 
