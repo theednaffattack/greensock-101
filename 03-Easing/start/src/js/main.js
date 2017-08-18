376fa40444f316a1c8b8ea28d147a3f6d35f2c18
Eddie Naff <eddienaff@gmail.com>
2017-08-17 20:52:47 -0700

    Finish easing demo

--- 03-Easing/start/src/js/main.js
+++ 03-Easing/start/src/js/main.js
@@ -1,10 +1,28 @@
-(function($) {
+// (function($) {
     
-	var img = $('img'),
-		h2 = $('h2');
+// 	var img = $('img'),
+// 		h2 = $('h2');
 
-	// Simple Tween
-	TweenLite.from(img, 1, {x: -200});
-	TweenLite.from(h2, 1, {autoAlpha: 0, delay: 1});
+// 	// Simple Tween
+// 	TweenLite.from(img, 1, {x: -200, ease:Power0.easeOut});
+// 	TweenLite.from(h2, 1, {autoAlpha: 0, delay: 1});
 
-})(jQuery);
+// })(jQuery);
+
+
+  const img = document.querySelector('img');
+  const h2 = document.querySelector('h2');
+
+
+  // Simple Tween
+  // TweenLite.from(img, 1, {x: -200, ease:Power0.easeOut});
+  TweenLite.from(h2, 1, {autoAlpha: 0, delay: 1});
+  TweenLite.from(img, 1, {x: -200, ease:Back.easeOut});
+  // TweenLite.from(img, 1, {x: -200, ease:Bounce.easeOut});
+  // TweenLite.from(img, 1, {x: -200, ease:Circ.easeOut});
+  // TweenLite.from(img, 1, {x: -200, ease:Elastic.easeOut});
+  // TweenLite.from(img, 1, {x: -200, ease:Expo.easeOut});
+
+  // TweenLite.from(img, 1, {x: -200, ease:RoughEase.ease.config({ template: Power0.easeOut })});
+  // TweenLite.from(img, 1, {x: -200, ease:SlowMo.easeOut}); // slowmo
+  // TweenLite.from(img, 1, {x: -200, ease:SteppedEase.easeOut}); // steppedease
