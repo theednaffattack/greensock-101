f4a207aa88a315fb788abed82428e8126fc10012
Eddie Naff <eddienaff@gmail.com>
2017-08-17 18:27:54 -0700

    Animate header to move down and fade out

--- 01-Selectors/start/src/js/main.js
+++ 01-Selectors/start/src/js/main.js
@@ -1,5 +1,13 @@
 (function($) {
     
 	// JS code goes here
-	alert(1);
+	// alert(1);
+  const header = $('#header');
+
+  // TwwenLite instructions(inside 'to' method):
+  // element to animate, duration in seconds, an object w/ what you want to animate
+  // 'TweenLite.to(' == the TweenLite module 'to' method, so a destination to animate to?
+  // 'header, 1' == animate the header, the animation time is one second
+  // {opacity: 0, y: 50} == move it 50 pixels down and to opacity 0 (fade out)
+  TweenLite.to(header, 1, {opacity: 0, y: 50});
 })(jQuery);
