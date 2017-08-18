ee1e1e2a4378b7603596b9ef7ed4cee3c7ac0e18
Eddie Naff <eddienaff@gmail.com>
2017-08-17 20:53:49 -0700

    Finish animating multiple objects

--- 05-Animating-Multiple-Objects/start/js/main.js
+++ 05-Animating-Multiple-Objects/start/js/main.js
@@ -1,1 +1,45 @@
-!function(e){var a=e("img"),o=e("h2"),r=e("h1"),t=e(".intro"),u=e("ul li"),s=new TimelineLite;s.from(r,.3,{y:-15,autoAlpha:0,ease:Power1.easeOut}).from(t,.3,{y:-15,autoAlpha:0,ease:Power1.easeOut},"-=0.15").from(a,.3,{y:-15,autoAlpha:0,ease:Power1.easeOut},"-=0.15").from(o,.3,{y:-15,autoAlpha:0,ease:Power1.easeOut},"-=0.15").from(u,.3,{y:-15,autoAlpha:0,ease:Power1.easeOut},"-=0.15")}(jQuery);
+!function(e){
+  // const a=e("img");
+  // const o=e("h2");
+  // const r=e("h1");
+  // const t=e(".intro");
+  // const u=e("ul li");
+  // const s=new TimelineLite;
+
+
+  let img = $("img");
+  let h2 = $("h2");
+  let h1 = $("h1");
+  let intro = $(".intro");
+  let listItems = $("ul li");
+  let tl = new TimelineLite; // it's common to name timeline 'tl'
+
+  // Animating a TimeLine
+  tl
+    .from(h1, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut, delay: 0.2})
+    .from(intro, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut, delay: 0.4})
+    .from(img, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut, delay: 0.8})
+    .from(h2, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut, delay: 0.9})
+    .from(listItem, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut, delay: 1});
+
+  // s.from(r,.3,{
+  //   y:-15,
+  //   autoAlpha:0,
+  //   ease:Power1.easeOut
+  // }).from(t,.3,{
+  //   y:-15,
+  //   autoAlpha:0,
+  //   ease:Power1.easeOut
+  // },"-=0.15").from(a,.3,{
+  //   y:-15,
+  //   autoAlpha:0,
+  //   ease:Power1.easeOut
+  // },"-=0.15").from(o,.3,{
+  //   y:-15,autoAlpha:0,
+  //   ease:Power1.easeOut
+  // },"-=0.15").from(u,.3,{
+  //   y:-15,
+  //   autoAlpha:0,
+  //   ease:Power1.easeOut
+  // },"-=0.15")
+}(jQuery);
--- 05-Animating-Multiple-Objects/start/src/js/main.js
+++ 05-Animating-Multiple-Objects/start/src/js/main.js
@@ -1,11 +1,26 @@
 (function($) {
     
-	var img = $('img'),
-		h2 = $('h2'),
-		h1 = $('h1'),
-		intro = $('.intro'),
-		listItem = $('ul li');
+	// var img = $('img'),
+	// 	h2 = $('h2'),
+	// 	h1 = $('h1'),
+	// 	intro = $('.intro'),
+	// 	listItem = $('ul li');
+
+
+
+  const img = $("img");
+  const h2 = $("h2");
+  const h1 = $("h1");
+  const intro = $(".intro");
+  const listItems = $("ul li");
+  const tl = new TimelineLite; // it's common to name timeline 'tl'
 
 	// Animating Multiple Objects
 
+  tl
+    .from(h1, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut})
+    .from(intro, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut}, '-=0.15')
+    .from(img, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut}, '-=0.15')
+    .from(h2, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut}, '-=0.15')
+    .from(listItem, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut}, '-=0.15');
 })(jQuery);
